ce420e2542983696ad92fc723184212d0d0b9334
Vinicius Trampusch <49486098+viniciustrampusch@users.noreply.github.com>
2020-10-28 05:07:44 -0300

    Update CalendarStatus.php

--- backend/app/Models/CalendarStatus.php
+++ backend/app/Models/CalendarStatus.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CalendarStatus extends Model
 {
+    use SoftDeletes;
+    
     protected $table = "calendar_status";
 }
